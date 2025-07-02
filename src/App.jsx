d402efe04f1cad203768b65dac4a2202a586c9a9
GopalYad <yadavgopal6675@gmail.com>
2025-07-02 16:19:52 +0530

    intializin shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 function App() {
 
   return (
     <>
      <h1 className=' min-w-lg w-md mx-auto mt-4 p-2 bg-blue-300 outline-1 hover:outline-2 hover:bg-blue-500 font-mono'>Initializing project with react , tailwind css , shadncn ui and Rapid API</h1>
+          <Button>Click me</Button>
     </>
   )
 }
