06800f621bc7233d527e0fb0206e445ba0d1731b
GopalYad <yadavgopal6675@gmail.com>
2025-07-02 15:31:53 +0530

    intialize project with reactjs vite tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1 className=' min-w-lg w-md mx-auto mt-4 p-2 bg-blue-300 outline-1 hover:outline-2 hover:bg-blue-500 font-mono'>Initializing project with react , tailwind css , shadncn ui and Rapid API</h1>
+    </>
+  )
+}
+
+export default App
